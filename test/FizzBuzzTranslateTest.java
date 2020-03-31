9c3f93ccd2273db6f3e1c3795c767295aebe032f
vanduc2514 <vanduc2514@gmail.com>
2020-03-31 18:18:38 +0700

    Sprint 2

--- test/FizzBuzzTranslateTest.java
+++ test/FizzBuzzTranslateTest.java
@@ -7,9 +7,18 @@ class FizzBuzzTranslateTest {
 
     @DisplayName("Test with 1")
     @Test
-    void translate() {
+    void testTranslateCase1() {
         int number = 1;
         String expectedResult = "N/A";
+        String actualResult = FizzBuzzTranslate.translate(number);
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @DisplayName("Test with 9")
+    @Test
+    void testTranslateCase2() {
+        int number = 9;
+        String expectedResult = "Fizz";
         String actualResult = FizzBuzzTranslate.translate(number);
         assertEquals(expectedResult,actualResult);
     }
